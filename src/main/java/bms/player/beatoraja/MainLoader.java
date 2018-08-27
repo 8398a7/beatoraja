f53bdf1466dd43ca37b27afe45a0c6097bf1644f
8398a7 <8398a7@gmail.com>
2018-08-27 22:11:05 +0900

    gradle wip

---
+++ src/main/java/bms/player/beatoraja/MainLoader.java
@@ -0,0 +1,197 @@
+package bms.player.beatoraja;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+
+import com.badlogic.gdx.Graphics;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import bms.player.beatoraja.PlayerResource.PlayMode;
+import bms.player.beatoraja.launcher.PlayConfigurationView;
+
+/**
+ * 起動用クラス
+ *
+ * @author exch
+ */
+public class MainLoader extends Application {
+	
+	private static final boolean ALLOWS_32BIT_JAVA = false;
+	
+	public static void main(String[] args) {
+		
+		if(!ALLOWS_32BIT_JAVA && !System.getProperty( "os.arch" ).contains( "64")) {
+			JOptionPane.showMessageDialog(null, "This Application needs 64bit-Java.", "Error", JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
+		}
+
+		Logger logger = Logger.getGlobal();
+		try {
+			logger.addHandler(new FileHandler("beatoraja_log.xml"));
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+
+		Path f = null;
+		PlayMode auto = null;
+		for (String s : args) {
+			if (s.startsWith("-")) {
+				if (s.equals("-a")) {
+					auto = PlayMode.AUTOPLAY;
+				}
+				if (s.equals("-p")) {
+					auto = PlayMode.PRACTICE;
+				}
+				if (s.equals("-r") || s.equals("-r1")) {
+					auto = PlayMode.REPLAY_1;
+				}
+				if (s.equals("-r2")) {
+					auto = PlayMode.REPLAY_2;
+				}
+				if (s.equals("-r3")) {
+					auto = PlayMode.REPLAY_3;
+				}
+				if (s.equals("-r4")) {
+					auto = PlayMode.REPLAY_4;
+				}
+				if (s.equals("-s")) {
+					auto = PlayMode.PLAY;
+				}
+			} else {
+				f = Paths.get(s);
+				if(auto == null) {
+					auto = PlayMode.PLAY;
+				}
+			}
+		}
+		
+		if(Files.exists(MainController.configpath) && (f != null || auto != null)) {
+			play(f, auto, true, null, null, f != null);			
+		} else {
+			launch(args);			
+		}
+	}
+
+	public static void play(Path f, PlayMode auto, boolean forceExit, Config config, PlayerConfig player, boolean songUpdated) {
+		if(config == null) {
+			config = Config.read();			
+		}
+
+		try {
+			MainController main = new MainController(f, config, player, auto, songUpdated);
+
+			LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+			cfg.width = config.getResolution().width;
+			cfg.height = config.getResolution().height;
+
+			// fullscreen
+			switch (config.getDisplaymode()) {
+				case FULLSCREEN:
+					cfg.fullscreen = true;
+					break;
+				case BORDERLESS:
+					System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
+					cfg.fullscreen = false;
+					break;
+				case WINDOW:
+					cfg.fullscreen = false;
+					break;
+			}
+			// vSync
+			cfg.vSyncEnabled = config.isVsync();
+			cfg.backgroundFPS = config.getMaxFramePerSecond();
+			cfg.foregroundFPS = config.getMaxFramePerSecond();
+			cfg.title = MainController.VERSION;
+			
+			cfg.audioDeviceBufferSize = config.getAudioDeviceBufferSize();
+			cfg.audioDeviceSimultaneousSources = config.getAudioDeviceSimultaneousSources();
+			cfg.forceExit = forceExit;
+			if(config.getAudioDriver() != Config.AUDIODRIVER_SOUND && config.getAudioDriver() != Config.AUDIODRIVER_AUDIODEVICE) {
+				LwjglApplicationConfiguration.disableAudio = true;				
+			}
+			// System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL",
+			// "true");
+			new LwjglApplication(main, cfg);
+
+//			Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+//
+//			final int w = (int) RESOLUTION[config.getResolution()].width;
+//			final int h = (int) RESOLUTION[config.getResolution()].height;
+//			if (config.isFullscreen()) {
+//				DisplayMode d = null;
+//				for (DisplayMode display : cfg.getDisplayModes()) {
+//					System.out.println("available DisplayMode : w - " + display.width + " h - " + display.height
+//							+ " refresh - " + display.refreshRate + " color bit - " + display.bitsPerPixel);
+//					if (display.width == w
+//							&& display.height == h
+//							&& (d == null || (d.refreshRate <= display.refreshRate && d.bitsPerPixel <= display.bitsPerPixel))) {
+//						d = display;
+//					}
+//				}
+//				if (d != null) {
+//					cfg.setFullscreenMode(d);
+//				} else {
+//					cfg.setWindowedMode(w, h);
+//				}
+//			} else {
+//				cfg.setWindowedMode(w, h);
+//			}
+//			// vSync
+//			cfg.useVsync(config.isVsync());
+//			cfg.setIdleFPS(config.getMaxFramePerSecond());
+//			cfg.setTitle(VERSION);
+//
+//			cfg.setAudioConfig(config.getAudioDeviceSimultaneousSources(), config.getAudioDeviceBufferSize(), 1);
+//
+//			new Lwjgl3Application(main, cfg);
+		} catch (Throwable e) {
+			e.printStackTrace();
+			Logger.getGlobal().severe(e.getClass().getName() + " : " + e.getMessage());
+		}
+	}
+
+	public static Graphics.DisplayMode[] getAvailableDisplayMode() {
+		return LwjglApplicationConfiguration.getDisplayModes();
+	}
+
+	public static Graphics.DisplayMode getDesktopDisplayMode() {
+		return LwjglApplicationConfiguration.getDesktopDisplayMode();
+	}
+
+	@Override
+	public void start(javafx.stage.Stage primaryStage) throws Exception {
+		Config config = Config.read();
+
+		try {
+			ResourceBundle bundle = ResourceBundle.getBundle("UIResources");
+			FXMLLoader loader = new FXMLLoader(
+					MainLoader.class.getResource("/fxml/launcher/PlayConfigurationView.fxml"), bundle);
+			VBox stackPane = (VBox) loader.load();
+			PlayConfigurationView bmsinfo = (PlayConfigurationView) loader.getController();
+			bmsinfo.setBMSInformationLoader(this);
+			bmsinfo.update(config);
+			Scene scene = new Scene(stackPane, stackPane.getPrefWidth(), stackPane.getPrefHeight());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle(MainController.VERSION + " configuration");
+			primaryStage.setOnCloseRequest((event) -> {
+				bmsinfo.exit();
+			});
+			primaryStage.show();
+
+		} catch (IOException e) {
+			Logger.getGlobal().severe(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/bms/player/beatoraja/TableDataAccessor.java
@@ -0,0 +1,289 @@
+package bms.player.beatoraja;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonWriter.OutputType;
+
+import bms.model.Mode;
+import bms.player.beatoraja.CourseData.TrophyData;
+import bms.player.beatoraja.song.SongData;
+import bms.table.BMSTableElement;
+import bms.table.Course;
+import bms.table.Course.Trophy;
+import bms.table.DifficultyTable;
+import bms.table.DifficultyTableElement;
+import bms.table.DifficultyTableParser;
+
+/**
+ * 難易度表データアクセス用クラス
+ * 
+ * @author exch
+ */
+public class TableDataAccessor {
+	
+	private String tabledir = "table";
+
+	public TableDataAccessor(String tabledir) {
+		this.tabledir = tabledir;
+	}
+
+	public void updateTableData(String[] urls) {
+		final ConcurrentLinkedDeque< Thread> tasks = new ConcurrentLinkedDeque<Thread>();
+		for (final String url : urls) {
+			Thread task = new Thread(() -> {
+                TableAccessor tr = new DifficultyTableAccessor(tabledir, url);
+                TableData td = tr.read();
+                if(td != null) {
+                    write(td);
+                }
+            });
+			tasks.add(task);
+			task.start();
+		}
+		
+		while(!tasks.isEmpty()) {
+			if(!tasks.getFirst().isAlive()) {
+				tasks.removeFirst();
+			} else {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 難易度表データをキャッシュする
+	 * 
+	 * @param td 難易度表データ
+	 */
+	public void write(TableData td) {
+		try {
+			td.shrink();
+			Json json = new Json();
+			json.setElementType(TableData.class, "folder", ArrayList.class);
+			json.setElementType(TableData.TableFolder.class, "songs", ArrayList.class);
+			json.setElementType(TableData.class, "course", ArrayList.class);
+			json.setElementType(CourseData.class, "trophy", ArrayList.class);
+			json.setOutputType(OutputType.json);
+			OutputStreamWriter fw = new OutputStreamWriter(new BufferedOutputStream(
+					new GZIPOutputStream(new FileOutputStream(tabledir + "/" + td.getName() + ".bmt"))), "UTF-8");
+			fw.write(json.prettyPrint(td));
+			fw.flush();
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 全てのキャッシュされた難易度表データを読み込む
+	 * 
+	 * @return 全てのキャッシュされた難易度表データ
+	 */
+	public TableData[] readAll() {
+		List<TableData> result = new ArrayList<TableData>();
+		try (DirectoryStream<Path> paths = Files.newDirectoryStream(Paths.get(tabledir))) {
+			for (Path p : paths) {
+				if (p.toString().endsWith(".bmt")) {
+					try {
+						Json json = new Json();
+						json.setIgnoreUnknownFields(true);
+						TableData td = json.fromJson(TableData.class,
+								new BufferedInputStream(new GZIPInputStream(Files.newInputStream(p))));
+						if(td != null && td.validate()) {
+							result.add(td);
+						}
+					} catch(Throwable e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result.toArray(new TableData[result.size()]);
+	}
+
+	/**
+	 * 指定のキャッシュされた難易度表データを読み込む
+	 * 
+	 * @param name 難易度表名
+	 * @return キャッシュされた難易度表データ。存在しない場合はnull
+	 */
+	public TableData read(String name) {
+		TableData td = null;
+		try (DirectoryStream<Path> paths = Files.newDirectoryStream(Paths.get(tabledir))) {
+			for (Path p : paths) {
+				if (p.getFileName().toString().equals(name + ".bmt")) {
+					try {
+						Json json = new Json();
+						json.setIgnoreUnknownFields(true);
+						td = json.fromJson(TableData.class,
+								new BufferedInputStream(new GZIPInputStream(Files.newInputStream(p))));
+						if(td == null || !td.validate()) {
+							td = null;
+						}
+						break;
+					} catch(Throwable e) {
+
+					}
+				}
+			}
+		} catch (IOException e) {
+
+		}
+		return td;
+	}
+
+	public static abstract class TableAccessor {
+
+		public final String name;
+
+		public TableAccessor(String name) {
+			this.name = name;
+		}
+
+		public abstract TableData read();
+		public abstract void write(TableData td);
+	}
+
+	public static class DifficultyTableAccessor extends TableAccessor {
+
+		private String tabledir;
+		private String url;
+
+		public DifficultyTableAccessor(String tabledir, String url) {
+			super(url);
+			this.tabledir = tabledir;
+			this.url = url;
+		}
+
+		@Override
+		public TableData read() {
+			DifficultyTableParser dtp = new DifficultyTableParser();
+			DifficultyTable dt = new DifficultyTable();
+			if (url.endsWith(".json")) {
+				dt.setHeadURL(url);
+			} else {
+				dt.setSourceURL(url);
+			}
+			try {
+				dtp.decode(true, dt);
+				TableData td = new TableData();
+				td.setUrl(url);
+				td.setName(dt.getName());
+				td.setTag(dt.getTag());
+				Mode defaultMode = dt.getMode() != null ? Mode.getMode(dt.getMode()) : null;
+				String[] levels = dt.getLevelDescription();
+				List<TableData.TableFolder> tdes = new ArrayList<>(levels.length);
+				for (String lv : levels) {
+					TableData.TableFolder tde = new TableData.TableFolder();
+					tde.setName(td.getTag() + lv);
+					List<SongData> hashes = new ArrayList<SongData>();
+					for (DifficultyTableElement dte : dt.getElements()) {
+						if (lv.equals(dte.getLevel())) {
+							SongData sd = toSongData(dte, defaultMode);
+							hashes.add(sd);
+						}
+					}
+					tde.setSong(hashes.toArray(new SongData[hashes.size()]));
+					tdes.add(tde);
+				}
+				td.setFolder(tdes.toArray(new TableData.TableFolder[tdes.size()]));
+
+				if (dt.getCourse() != null && dt.getCourse().length > 0) {
+					List<CourseData> gname = new ArrayList<CourseData>();
+					for (Course[] course : dt.getCourse()) {
+						for (Course g : course) {
+							CourseData cd = new CourseData();
+							cd.setName(g.getName());
+							SongData[] songs = new SongData[g.getCharts().length];
+							for(int i = 0;i < songs.length;i++) {
+								songs[i] = toSongData(g.getCharts()[i], defaultMode);
+							}
+							cd.setSong(songs);
+							List<CourseData.CourseDataConstraint> l = new ArrayList<>();
+							for(int i = 0;i < g.getConstraint().length;i++) {
+								for (CourseData.CourseDataConstraint constraint : CourseData.CourseDataConstraint.values()) {
+									if (constraint.name.equals(g.getConstraint()[i])) {
+										l.add(constraint);
+										break;
+									}
+								}
+							}
+							cd.setConstraint(l.toArray(new CourseData.CourseDataConstraint[l.size()]));
+							if (g.getTrophy() != null) {
+								List<TrophyData> tr = new ArrayList<TrophyData>();
+								for (Trophy trophy : g.getTrophy()) {
+									TrophyData t = new TrophyData();
+									t.setName(trophy.getName());
+									t.setMissrate((float) trophy.getMissrate());
+									t.setScorerate((float) trophy.getScorerate());
+									tr.add(t);
+								}
+								cd.setTrophy(tr.toArray(new TrophyData[tr.size()]));
+							}
+							gname.add(cd);
+						}
+					}
+
+					td.setCourse(gname.toArray(new CourseData[gname.size()]));
+				}
+				if(td == null || !td.validate()) {
+					throw new RuntimeException("難易度表の値が不正です");
+				}
+				return td;
+			} catch (Throwable e) {
+				e.printStackTrace();
+				Logger.getGlobal().warning("難易度表 - "+url+" の読み込み失敗。");
+			}
+			return null;
+		}
+
+		@Override
+		public void write(TableData td) {
+			new TableDataAccessor(tabledir).write(td);
+		}
+	}
+	
+	private static SongData toSongData(BMSTableElement te, Mode defaultMode) {
+		SongData song = new SongData();
+		if(te.getMD5() != null) {
+			song.setMd5(te.getMD5().toLowerCase());
+		}
+		if(te.getSHA256() != null) {
+			song.setSha256(te.getSHA256().toLowerCase());
+		}
+		song.setTitle(te.getTitle());
+		song.setArtist(te.getArtist());
+		Mode mode = te.getMode() != null ? Mode.getMode(te.getMode()) : null;
+		song.setMode(mode != null ? mode.id : (defaultMode != null ? defaultMode.id : 0));
+		song.setUrl(te.getURL());
+		song.setIpfs(te.getIPFS());
+		song.setOrg_md5(te.getParentHash());
+		if(te instanceof DifficultyTableElement) {
+			DifficultyTableElement dte = (DifficultyTableElement) te;
+			song.setAppendurl(dte.getAppendURL());
+			song.setAppendIpfs(dte.getAppendIPFS());
+		}
+		
+		return song;
+	}
+}
